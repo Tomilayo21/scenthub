f4df8452e6790a4bbd5897bfba78b905f1fbd4c3
Tomilayo21 <cusceda@yahoo.com>
2026-07-25 01:17:02 +0100

    First Commit

--- app/privacy-policy/page.jsx
+++ app/privacy-policy/page.jsx
@@ -8,7 +8,7 @@ export default function PrivacyPolicy() {
         <>
             <Navbar />
 
-            <main className="pt-32 pb-24 bg-white mt-8 md:mt-8">
+            <main className="pt-32 pb-24 bg-white">
                 <div className="max-w-5xl mx-auto px-6 md:px-12 lg:px-16 py-4 md:py-6">
 
                     {/* Header */}
